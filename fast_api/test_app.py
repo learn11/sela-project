77a41ce5b48293b84b670f5ac46fbea0139ae9e9
edmonp173 <edmonp173@gmail.com>
2024-06-14 02:07:14 +0300

    jenkins

--- fast_api/test_app.py
+++ fast_api/test_app.py
@@ -4,7 +4,7 @@ from app import app
 client = TestClient(app)
 
 def test_get_customers():
-    response = client.get("/customers")
+    response = client.get("/costumers")
     assert response.status_code == 200
 
 def test_get_products():
@@ -12,6 +12,6 @@ def test_get_products():
     assert response.status_code == 200
 
 def test_create_customer():
-    response = client.post("/input", json={"name": "John", "mail": "john@example.com", "phone": "123456789"})
+    response = client.post("/input_product", json={"name": "John", "mail": "john@example.com", "phone": "123456789"})
     assert response.status_code == 200
     assert response.json() == {"message": "Customer created successfully."}
